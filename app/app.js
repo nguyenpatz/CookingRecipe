e783c4c82bd011f08f989c902c7b6236f57bc1f3
nguyenpatz <nguyen79248@st.vimaru.edu.vn>
2022-02-22 22:34:46 +0700

    update food in menu

--- app/app.js
+++ app/app.js
@@ -15,6 +15,7 @@ const data_recipe = [{
     noidung: "Đang cập nhật",
     loai: "kho",
     tenloai: "Món kho",
+    src: "./images/foods/thitkho.jpg",
     nguyenlieu: ["Thịt ba chỉ hay thịt chân giò 500 gr",
         "Trứng vịt luộc 5 quả",
         "Nước dừa 400 ml",
@@ -31,6 +32,7 @@ const data_recipe = [{
     noidung: "Đang cập nhật",
     loai: "xao",
     tenloai: "Món xào",
+    src: "./images/foods/suonxao.jpg",
     nguyenlieu: [
         " Sườn heo non 500 gr",
         "Ớt chuông 3 trái",
@@ -53,6 +55,7 @@ const data_recipe = [{
     noidung: "Đang cập nhật",
     loai: "xao",
     tenloai: "Món xào",
+    src: "./images/foods/raumuongxao.jpg",
     nguyenlieu: [
         "Rau muống 500 gr",
         "Tỏi 2 củ",
@@ -68,6 +71,7 @@ const data_recipe = [{
     noidung: "Đang cập nhật",
     loai: "luoc",
     tenloai: "Món luộc",
+    src: "./images/foods/thitluoc.jpg",
     nguyenlieu: [
         "Thịt nạc",
         "Thịt mỡ",
@@ -80,11 +84,19 @@ const data_recipe = [{
 // display Menu with object
 //container button
 const buttonContainter = document.querySelector(".button__container");
+const foodContainer = document.querySelector(".section__center");
+
 window.addEventListener("DOMContentLoaded", function() {
     
+    displayFood(data_recipe);
+    displayButton(data_recipe);
+
+});
+
+function displayButton(data) {
     // chỉ lấy mỗi loai để hiển thị tên lên màn hình thôi 
     const initialValue = ["Tất cả"];
-    const nameButtons = data_recipe.reduce(function(prev, next) {
+    const nameButtons = data.reduce(function(prev, next) {
         if(!prev.includes(next.tenloai)) {
             prev.push(next.tenloai);
         }
@@ -101,5 +113,18 @@ window.addEventListener("DOMContentLoaded", function() {
     buttonContainter.innerHTML = filter_button;
     // sau khi có được tên loai thì bắt đầu thực hiện sự kiện 
     // khi click vào 1 loại sẽ chỉ hiển thị món ăn có liên quan tới loại đó 
+}
 
-});
+function displayFood(data) {
+    const showFood = data.map(function(element) {
+        return `<div class="grid__item">
+                <img class="grid__image" src="${element.src}" alt="${element.ten}">
+                <div>
+                    <p class="food__name">${element.ten}</p>
+                    <p class="food__title">${element.tieude}</p>
+                </div>
+            </div>`;
+    }).join("");
+
+    foodContainer.innerHTML = showFood;
+}
